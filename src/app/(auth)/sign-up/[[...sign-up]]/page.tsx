5529d56cc5d791837a6cf2277f60baf8aa43e3cb
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-11-05 22:52:24 +0100

    style clerk

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <SignUp appearance={{ variables: { colorPrimary: '#0F172A' } }} />
+    </div>
+  )
 }
