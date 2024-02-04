b1983adde7f9e9ff019809ad51185993127d6137
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-04 18:30:49 +0100

    fixing the ai chat bot in pinecone

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,53 +3,56 @@ import openai, { getEmbedding } from '@/lib/openai'
 import { notesIndex } from '@/lib/pinecone'
 import { auth } from '@clerk/nextjs/server'
 import { ChatCompletionMessage } from 'openai/resources/index.mjs'
-import {OpenAIStream, StreamingTextResponse} from "ai"
+import { OpenAIStream, StreamingTextResponse } from 'ai'
 export async function POST(req: Request) {
   try {
     const body = await req.json()
-
     const messages: ChatCompletionMessage[] = body.messages
-    const messagesTrunc = messages.slice(-6)
-    const embbeding = await getEmbedding(
-      messagesTrunc.map((m) => m.content).join('\n')
+
+    const messagesTruncated = messages.slice(-6)
+
+    const embedding = await getEmbedding(
+      messagesTruncated.map((message) => message.content).join('\n')
     )
+
     const { userId } = auth()
 
-    const vectorQuery = await notesIndex.query({
-      vector: embbeding,
-      topK: 1,
+    const vectorQueryResponse = await notesIndex.query({
+      vector: embedding,
+      topK: 4,
       filter: { userId },
     })
 
-    const relevanteNote = await db.note.findMany({
-     where:{
-      id:{
-       in: vectorQuery.matches.map((m) => m.id),
-      }
-     }
+    const relevantNotes = await db.note.findMany({
+      where: {
+        id: {
+          in: vectorQueryResponse.matches.map((match) => match.id),
+        },
+      },
     })
 
-    console.log("relevant",relevanteNote)
+    console.log('Relevant notes found: ', relevantNotes)
 
-    const systemMsg: ChatCompletionMessage = {
+    const systemMessage: ChatCompletionMessage = {
       role: 'system',
       content:
-        'You are a helpful assistant that writes notes for the user and answer their questions.' +
+        "You are an intelligent note-taking app. You answer the user's question based on their existing notes. " +
         'The relevant notes for this query are:\n' +
-        relevanteNote
+        relevantNotes
           .map((note) => `Title: ${note.title}\n\nContent:\n${note.content}`)
           .join('\n\n'),
     }
+
     const response = await openai.chat.completions.create({
-     model:"gpt-3.5-turbo",
-     stream:true,
-     messages: [systemMsg, ...messages],
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: [systemMessage, ...messagesTruncated],
     })
 
-    const  stream = OpenAIStream(response)
+    const stream = OpenAIStream(response)
     return new StreamingTextResponse(stream)
   } catch (error) {
-    console.log(error)
-    return new Response('Internal Server Error', { status: 500 })
+    console.error(error)
+    return Response.json({ error: 'Internal server error' }, { status: 500 })
   }
 }
